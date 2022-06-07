27ec6a199be40fa3c56bff816d03638f4da8e056
Toshib061 <59652027+Toshib061@users.noreply.github.com>
2022-06-08 04:36:45 +0600

    Add files via upload

---
+++ Hackerrank Sql/The Count Function.sql
@@ -0,0 +1,6 @@
+/*Query a count of the number of cities in CITY having a Population larger than 100,000.
+
+Input Format
+
+The CITY table is described as follows:*/
+SELECT COUNT(POPULATION) FROM CITY WHERE POPULATION>100000;
